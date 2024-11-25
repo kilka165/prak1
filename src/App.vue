3f92d5caec751a74f51fc7b8e262c606476301cb
ИС-23-19Б <@POLYTECH2.KZ>
2024-11-25 13:10:42 +0500

    feat: finished logic app

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,111 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 const products = ref([
-{
-  id: Date.now(),
-  name: "HP",
-  data: "25.11.2024",
-  count: 15,
-  price: 100000,
-},
-{
-  id: Date.now(),
-  name: "asus",
-  data: "25.11.2024",
-  count: 152,
-  price: 150000,
-},
-{
-  id: Date.now(),
-  name: "lenovo",
-  data: "25.11.2024",
-  count: 5,
-  price: 100003,
-},
-{
-  id: Date.now(),
-  name: "tesla",
-  data: "25.11.2024",
-  count: 11,
-  price: 102000,
-},
+  {
+    id: 1,
+    name: 'Tesla',
+    date: "25.11.2024",
+    count: 10,
+    price: 4000,
+  },
+  {
+    id: 2,
+    name: 'HP',
+    date: "25.11.2024",
+    count: 18,
+    price: 1500,
+  },
+  {
+    id: 3,
+    name: 'Acer',
+    date: "25.11.2024",
+    count: 20,
+    price: 2500,
+  },
+  {
+    id: 4,
+    name: 'Asus',
+    date: "25.11.2024",
+    count: 25,
+    price: 3500,
+  },
 ]);
+
+const name = ref('');
+const date = ref('');
+const count = ref(1);
+const price = ref(0);
+
+const addProduct = () => {
+  if (name.value && date.value && count.value && price.value) {
+    products.value.push({
+    id: Date.now(),
+    name: name.value,
+    date: date.value,
+    count: count.value,
+    price: price.value,
+    })
+  }
+}
+const removeProduct = (id) => {
+  products.value = products.value.filter((products) => products.id != id)
+}
+const totalSum = computed (() => {
+  return products.value.reduce((sum,product) => sum + (product.price * product.count), 0);
+}) 
 </script>
 
 <template>
+  <div class="container">
+    <div class="row">
 
-<div class="contair">
- <div class="rol">
+    <div class="col">
+      <h1 class="text-center my-3">Учет товаров</h1>
+
+      <div class="mb-3">
+  <label for="name" class="form-label">Название</label>
+  <input type="text" v-model="name"  class="form-control" id="name">
+  </div>
+
   <div class="mb-3">
-  <label for="name" class="form-label">Продукт</label>
-  <input type="text" class="form-control" id="name">
- </div>
- <div class="mb-3">
-  <label for="data" class="form-label">Дата</label>
-  <input type="data" class="form-control" id="data">
- </div>
- <div class="mb-3">
+  <label for="date" class="form-label">Дата</label>
+  <input type="date" v-model="date" class="form-control" id="date">
+  </div>
+
+  <div class="mb-3">
   <label for="count" class="form-label">Количество</label>
-  <input type="number" class="form-control" id="count">
- </div>
- <div class="mb-3">
-  <label for="count" class="form-label">Цена за единицу товара</label>
-  <input type="number" class="form-control" id="count">
- </div>
- <button type="button" class="btn btn-primary">Добавить</button>
- </div>
- <div class="row row-cols-1 row-cols-md-3 g-4">
+  <input type="number" v-model="count" class="form-control" id="count">
+  </div>
+
+  <div class="mb-3">
+  <label for="price" class="form-label">Цена</label>
+  <input type="number" v-model="price" class="form-control" id="price">
+  </div>
+  <button type="button" class="btn btn-success" @click="addProduct">Добавить</button>
+
+</div>
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col" v-for="products in products">
-    <div class="card">
+    <div class="card h-100">
       <div class="card-body">
         <h5 class="card-title">{{ products.name }}</h5>
-        <p class="card-text">{{ products.price }}</p>
+        <p class="card-text">${{ products.price }}</p>
         <p class="card-text">{{ products.count }}</p>
         <p class="card-text">{{ products.date }}</p>
-        <button type="button" class="btn btn-danger">Удалить</button>
+      </div>
+      <div class="card-footer text-end">
+        <button class="btn btn-outline-danger" @click="removeProduct(products.id)">Удалить</button>
       </div>
     </div>
   </div>
 </div>
-<h4 class="text-end">Общая сумма товаров: 1000$</h4>
+
+</div>
+<div class="row my-4">
+<div class="col">
+<h3 class="text-end">Общая сумма товаров: ${{ totalSum }}</h3>
+</div>
+</div>
 </div>
 </template>
-
-
