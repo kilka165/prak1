2f8bedbbee25ae69544c9a8ea7a50508996127f6
ИС-23-19Б <@POLYTECH2.KZ>
2024-11-25 10:35:48 +0500

    feat: add card

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,21 @@
   <label for="count" class="form-label">Цена за единицу товара</label>
   <input type="number" class="form-control" id="count">
  </div>
- </div>
  <button type="button" class="btn btn-primary">Добавить</button>
+ </div>
+ <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">HP</h5>
+        <p class="card-text">$1000</p>
+        <p class="card-text">10шт</p>
+        <p class="card-text">25.11.2024</p>
+        <button type="button" class="btn btn-danger">Удалить</button>
+      </div>
+    </div>
+  </div>
+</div>
 </div>
 </template>
 
