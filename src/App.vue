1fe3c4a2a4732c2e85d8cafc3e5421e7c3e84d14
ИС-23-19Б <@Polytech2.kz>
2024-11-25 10:01:28 +0500

    project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+ <button type="button" class="btn btn-primary">Primary</button>
+<div class="contair">
+ <div class="rol">
+  <label for="" class="" ></label>
+
+ </div>
+</div>
+</template>
+
+
