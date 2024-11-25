f2055e0df084bc00a18dbc9ec2c7ad8ebc9e3c9f
ИС-23-19Б <@POLYTECH2.KZ>
2024-11-25 10:25:01 +0500

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,27 @@
 </script>
 
 <template>
- <button type="button" class="btn btn-primary">Primary</button>
+
 <div class="contair">
  <div class="rol">
-  <label for="" class="" ></label>
-
+  <div class="mb-3">
+  <label for="name" class="form-label">Продукт</label>
+  <input type="text" class="form-control" id="name">
  </div>
+ <div class="mb-3">
+  <label for="data" class="form-label">Дата</label>
+  <input type="data" class="form-control" id="data">
+ </div>
+ <div class="mb-3">
+  <label for="count" class="form-label">Количество</label>
+  <input type="number" class="form-control" id="count">
+ </div>
+ <div class="mb-3">
+  <label for="count" class="form-label">Цена за единицу товара</label>
+  <input type="number" class="form-control" id="count">
+ </div>
+ </div>
+ <button type="button" class="btn btn-primary">Добавить</button>
 </div>
 </template>
 
